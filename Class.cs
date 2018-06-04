124a937e0f4352bd2236da4b0b1cfd083e3c4ea5
saririan <39909363+saririan@users.noreply.github.com>
2018-06-04 19:25:14 +0100

    first upload

---
+++ Class.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PhoneNumber
+    {
+        public string Number { get; set; }
+        public bool Active { get; set; }
+        public PhoneNumber(string num, bool act)
+        {
+            Number = num;
+            Active = act;
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cName"></param>
+        /// <param name="cPhoneNum"></param>
+        public Customer(string cName, PhoneNumber cPhoneNum)
+        {
+            Name = cName;
+            List<PhoneNumber> PhoneNumbers = new List<PhoneNumber>();
+            PhoneNumbers.Add(cPhoneNum);
+            CusPhoneNumbers = PhoneNumbers;
+
+        }
+
+        public Customer()
+        {
+        }
+        public Customer(string cName,List<PhoneNumber> cPhoneNumList)
+        {
+            Name = cName;           
+            CusPhoneNumbers = cPhoneNumList;
+        }
+        public string Name { get; set; }
+     
+        public List<PhoneNumber> CusPhoneNumbers { get; set; }   
+
+        public void addPhone(PhoneNumber pn)
+        {
+            CusPhoneNumbers.Add(pn);
+        }
+    }
+}
---
+++ Controllers/PhonesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Phones")]
+    public class PhonesController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+
+        public PhonesController(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+        // GET api/phones
+        [HttpGet]
+        public JsonResult Get()
+        {
+           
+            string json = System.IO.File.ReadAllText(_hostingEnvironment.ContentRootPath +  "\\data\\path.txt");
+            List<Customer> items = JsonConvert.DeserializeObject<List<Customer>>(json);
+
+
+            List<string> PhoneList = new List<string>();
+
+            List<PhoneNumber> phones = new List<PhoneNumber>();
+
+            //List<List<PhoneNumber>> cc = items.Select(l => l.CusPhoneNumbers).ToList();
+            foreach (Customer x in items)
+            {
+                foreach(PhoneNumber phone in x.CusPhoneNumbers)
+                {
+                    PhoneList.Add(phone.Number);
+                }
+                
+            }
+
+            return Json(PhoneList);
+        }
+    }
+}
